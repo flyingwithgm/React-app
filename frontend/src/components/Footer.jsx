c641c0d9426bdbc0e1da956306d374cafe1f11f1
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-20 08:24:29 +0000

    Update Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white p-4 text-center">
+      © 2025 GM73 | The Flying Engineer. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
