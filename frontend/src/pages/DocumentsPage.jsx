450e114d1c038ff5fe79d83ea06d0e77c8a81c71
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-20 08:29:52 +0000

    Update DocumentsPage.jsx

---
+++ frontend/src/pages/DocumentsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DocumentsOrganizer from '../components/DocumentsOrganizer'
+
+const DocumentsPage = () => {
+  return (
+    <div>
+      <DocumentsOrganizer />
+    </div>
+  )
+}
+
+export default DocumentsPage
