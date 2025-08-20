984b9714a897eb09ebabf6197b5430351f4c666e
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-20 08:22:44 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Dashboard from './pages/Dashboard'
+import ResumePage from './pages/ResumePage'
+import EssayPage from './pages/EssayPage'
+import DocumentsPage from './pages/DocumentsPage'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/resume" element={<ResumePage />} />
+            <Route path="/essay" element={<EssayPage />} />
+            <Route path="/documents" element={<DocumentsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
