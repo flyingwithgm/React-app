62f03b1e8a7f73a49f32571168f83b84ede39b1a
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-20 08:28:43 +0000

    Update ResumePage.jsx

---
+++ frontend/src/pages/ResumePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResumeGenerator from '../components/ResumeGenerator'
+
+const ResumePage = () => {
+  return (
+    <div>
+      <ResumeGenerator />
+    </div>
+  )
+}
+
+export default ResumePage
